9b66efc4a4d8b9dd1c6e096b663e024fda23ba66
shi ren <shiren@wangwang.local>
2017-11-01 19:36:32 +0800

    Initial Commit

---
+++ NiuLangSport/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NiuLangSport
+//
+//  Created by shi ren on 2017/11/1.
+//  Copyright © 2017年 shi ren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
